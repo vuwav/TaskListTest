a28af3ed272beff7688b2c0fb131b5d9f15ba9df
vuwave <voltornistvv@gmail.com>
2020-11-05 11:16:04 +0500

    add migrations for users and tasks

---
+++ database/migrations/2020_11_04_194147_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->integer('priority');
+            $table->integer('status');
+            $table->integer('manager_id');
+            $table->integer('worker_id');
+            $table->timestamp('done_at')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
