4f84e050dac8d2570c5a1d290a8842da7e335da8
vuwave <voltornistvv@gmail.com>
2020-11-08 23:54:36 +0500

    add task routes

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,12 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('/login', 'AuthController@login');
     $router->post('/register', 'AuthController@register');
 
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->get('/task', 'TaskController@index');
+        $router->get('/task/{id}', 'TaskController@show');
+        $router->post('/task', 'TaskController@store');
+        $router->put('/task/{id}', 'TaskController@update');
+        $router->delete('/task/{id}', 'TaskController@destroy');
+    });
+
 });
