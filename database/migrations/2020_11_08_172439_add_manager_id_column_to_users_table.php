7f6e3fe88cadcecdb247bea129066627f2cb307c
vuwave <voltornistvv@gmail.com>
2020-11-09 11:02:43 +0500

    add migration ( manager_id column to user table )

---
+++ database/migrations/2020_11_08_172439_add_manager_id_column_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddManagerIdColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('manager_id')->after('id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('manager_id');
+        });
+    }
+}
