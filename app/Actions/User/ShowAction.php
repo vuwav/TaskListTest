239898e4d56a814cec9095657ec4f7af83d0b43d
vuwave <voltornistvv@gmail.com>
2020-11-11 09:50:56 +0500

    fix: formatting

--- app/Actions/User/ShowAction.php
+++ app/Actions/User/ShowAction.php
@@ -10,10 +10,12 @@ class ShowAction
 {
     public function __invoke(Request $request): array
     {
-       $data = ['user' => $request->user()];
-       if($request->user()->role === User::ROLE_MANAGER){
-           $data['workers'] = $request->user()->workers()->get(['id','name'])->map(fn($worker) => ['text' => $worker['name'], 'value' => $worker['id']] );
-       }
-       return [$data, 200];
+        $data = ['user' => $request->user()];
+        if ($request->user()->role === User::ROLE_MANAGER) {
+            $data['workers'] = $request->user()->workers()
+                ->get(['id', 'name'])
+                ->map(fn($worker) => ['text' => $worker['name'], 'value' => $worker['id']]);
+        }
+        return [$data, 200];
     }
 }
